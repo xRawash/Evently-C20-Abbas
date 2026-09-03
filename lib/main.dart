6b9363c6a8b5ad1caa64f2cba7c1c879c041c529
muhammed saad <moosaad@gmail.com>
2026-09-03 16:49:16 +0300

    Initialize Project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Evently());
+}
+
+class Evently extends StatelessWidget {
+  const Evently({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
